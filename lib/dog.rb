4d1887944994bdbe8e1b2e48e34bb59bec4c1bda
GAierken <g.aierken@gmail.com>
2019-11-02 15:58:14 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-  # code goes here
-end
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+  def initialize (name, owner)
+     @name = name
+     @owner = owner
+     @mood = "nervous"
+
+     @@all << self
+  end ## ini
+  
+
+  def self.all
+      @@all
+  end ## all
+end ##Dog
