168426c06af28adc672563820b674e831c5d6cef
ozubergz <jmin499@gmail.com>
2019-11-03 12:19:06 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
-  # code goes here
+  
+  #getter method
+  attr_reader :name
+
+  #getter and setter method
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+  def initialize(dog_name, owner)
+    @name = dog_name
+    @owner = owner
+    @mood = "nervous"
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def remove_owner
+    remove_instance_variable(:@owner)
+  end
+
 end
